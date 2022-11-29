c40273161a0f6a82e3c87d438d41930b79284dc3
Marco Belladelli <mbellade@redhat.com>
2022-11-29 14:57:15 +0100

    HHH-15734 Add test case for issue

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,22 @@
 package org.hibernate.bugs;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -28,11 +38,95 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void addOrphanNodeInBetweenTest() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
+
+		// ()
+		{
+			entityManager.getTransaction().begin();
+			Node a = node( "a" );
+			Node b = node( "b" );
+			bind( a, b );
+			entityManager.persist( a );
+			entityManager.getTransaction().commit();
+		}
+
+		// (a (b))
+		{
+			entityManager.getTransaction().begin();
+			Node a = entityManager.find( Node.class, "a" );
+			Node b = entityManager.find( Node.class, "b" );
+			unbind( a, b );
+			Node x = node( "x" );
+			bind( a, x );
+			bind( x, b );
+			entityManager.getTransaction().commit();
+		}
+
+		// (a (x (b)))
+		{
+			entityManager.getTransaction().begin();
+			Node a = entityManager.find(Node.class, "a");
+			Node b = entityManager.find(Node.class, "b");
+			Node x = entityManager.find(Node.class, "x");
+			assertNotNull(a);
+			assertNotNull(b);
+			assertNotNull(x);
+			entityManager.getTransaction().commit();
+		}
+
 		entityManager.close();
+	}
+
+	private Node node(String id) {
+		Node node = new Node();
+		node.setId(id);
+		node.setParent(null);
+		node.setChildren(new HashSet<>());
+		return node;
+	}
+
+	private void bind(Node parent, Node child) {
+		parent.getChildren().add(child);
+		child.setParent(parent);
+	}
+
+	private void unbind(Node parent, Node child) {
+		parent.getChildren().remove(child);
+		child.setParent(null);
+	}
+
+	@Entity
+	public static class Node {
+		@Id
+		private String id;
+		@ManyToOne
+		private Node parent;
+		@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+		private Set<Node> children;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public Node getParent() {
+			return parent;
+		}
+
+		public void setParent(Node parent) {
+			this.parent = parent;
+		}
+
+		public Set<Node> getChildren() {
+			return children;
+		}
+
+		public void setChildren(Set<Node> children) {
+			this.children = children;
+		}
 	}
 }
