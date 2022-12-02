b42fca22fba5a25cc68c67156340df10b4b37187
Marco Belladelli <mbellade@redhat.com>
2022-12-02 17:12:48 +0100

    HHH-15742 Add test case for issue

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,23 @@
 package org.hibernate.bugs;
 
+import java.time.Year;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -18,6 +29,14 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		EntityA entityA = new EntityA( 1, Year.parse( "2022" ));
+		entityManager.persist( entityA );
+
+		entityManager.getTransaction().commit();
+
 	}
 
 	@After
@@ -31,8 +50,64 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		TypedQuery<EntityA> query = entityManager.createQuery(
+				"from entitya a where :year is null or a.year = :year",
+				EntityA.class
+		);
+		query.setParameter( "year", Year.parse( "2022" ) );
+
+		EntityA entityA = query.getSingleResult();
+		assertEquals(1, entityA.getId());
+		assertEquals("2022", entityA.getYear().toString());
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "entitya")
+	public static class EntityA {
+		@Id
+		private Integer id;
+
+		@Column(name = "year_attribute")
+		@Convert(converter = YearConverter.class)
+		private Year year;
+
+		public EntityA() {
+		}
+
+		public EntityA(Integer id, Year year) {
+			this.id = id;
+			this.year = year;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public Year getYear() {
+			return year;
+		}
+
+		public void setYear(Year year) {
+			this.year = year;
+		}
+	}
+
+	@Converter
+	public static class YearConverter implements AttributeConverter<Year, String> {
+		@Override
+		public String convertToDatabaseColumn(Year attribute) {
+			return attribute != null ? attribute.toString() : null;
+		}
+		@Override
+		public Year convertToEntityAttribute(String dbData) {
+			return dbData != null ? Year.parse( dbData ) : null;
+		}
 	}
 }
