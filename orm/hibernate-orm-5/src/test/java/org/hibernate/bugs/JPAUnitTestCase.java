e5974157c6cadf8f189edb8f22a277a15bcccbb5
Marco Belladelli <mbellade@redhat.com>
2022-12-23 09:33:39 +0100

    HHH-15864 Add test for issue

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,7 +1,17 @@
 package org.hibernate.bugs;
 
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Persistence;
 
 import org.junit.After;
@@ -31,8 +41,100 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		// insert
+
+		ParentEntity parentEntity = new ParentEntity(
+				1,
+				new ChildEntityWrapper( Collections.singletonList( new ChildEntity() ) )
+		);
+		entityManager.persist( parentEntity );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+
+		// delete
+
+		EntityManager em2 = entityManagerFactory.createEntityManager();
+		em2.getTransaction().begin();
+
+		ParentEntity foundEntity = em2.find( ParentEntity.class, 1 );
+		em2.remove( foundEntity );
+
+		em2.getTransaction().commit();
+		em2.close();
+	}
+
+	@Entity
+	public static class ChildEntity {
+		@Id
+		@GeneratedValue
+		private int id;
+
+		public ChildEntity() {
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+	@Embeddable
+	public static class ChildEntityWrapper {
+		@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+		@JoinColumn(name = "parent_entity_id", referencedColumnName = "id")
+		private List<ChildEntity> childEntities;
+
+		public ChildEntityWrapper() {
+		}
+
+		public ChildEntityWrapper(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
+
+		public List<ChildEntity> getChildEntities() {
+			return childEntities;
+		}
+
+		public void setChildEntities(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
+	}
+
+	@Entity
+	public static class ParentEntity {
+		@Id
+		private int id;
+
+		@Embedded
+		private ChildEntityWrapper childEntityWrapper = new ChildEntityWrapper();
+
+		public ParentEntity() {
+		}
+
+		public ParentEntity(int id, ChildEntityWrapper childEntityWrapper) {
+			this.id = id;
+			this.childEntityWrapper = childEntityWrapper;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public ChildEntityWrapper getChildEntityWrapper() {
+			return childEntityWrapper;
+		}
+
+		public void setChildEntityWrapper(ChildEntityWrapper childEntityWrapper) {
+			this.childEntityWrapper = childEntityWrapper;
+		}
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,7 +1,19 @@
 package org.hibernate.bugs;
 
+import java.util.Collections;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Persistence;
 
 import org.junit.After;
@@ -31,8 +43,97 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		ParentEntity parentEntity = new ParentEntity(
+				new ChildEntityWrapper( Collections.singletonList( new ChildEntity() ) )
+		);
+		entityManager.persist( parentEntity );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+
+		// delete
+
+		EntityManager em2 = entityManagerFactory.createEntityManager();
+		em2.getTransaction().begin();
+
+		ParentEntity foundEntity = em2.find( ParentEntity.class, 1 );
+		em2.remove( foundEntity );
+
+		em2.getTransaction().commit();
+		em2.close();
+	}
+
+	@Entity
+	public static class ChildEntity {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+
+		public ChildEntity() {
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+	@Embeddable
+	public static class ChildEntityWrapper {
+		@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+		@JoinColumn(name = "parent_entity_id", referencedColumnName = "id")
+		private List<ChildEntity> childEntities;
+
+		public ChildEntityWrapper() {
+		}
+
+		public ChildEntityWrapper(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
+
+		public List<ChildEntity> getChildEntities() {
+			return childEntities;
+		}
+
+		public void setChildEntities(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
+	}
+
+	@Entity
+	public static class ParentEntity {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+
+		@Embedded
+		private ChildEntityWrapper childEntityWrapper = new ChildEntityWrapper();
+
+		public ParentEntity() {
+		}
+
+		public ParentEntity(ChildEntityWrapper childEntityWrapper) {
+			this.childEntityWrapper = childEntityWrapper;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public ChildEntityWrapper getChildEntityWrapper() {
+			return childEntityWrapper;
+		}
+
+		public void setChildEntityWrapper(ChildEntityWrapper childEntityWrapper) {
+			this.childEntityWrapper = childEntityWrapper;
+		}
 	}
 }
