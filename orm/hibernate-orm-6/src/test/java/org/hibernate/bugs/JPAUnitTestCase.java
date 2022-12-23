16b4d8019dabafd65a7f3a172077e173c802d88b
Marco Belladelli <mbellade@redhat.com>
2022-12-23 12:14:38 +0100

    HHH-15864 Add test for issue

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,7 +1,20 @@
 package org.hibernate.bugs;
 
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Persistence;
 
 import org.junit.After;
@@ -28,11 +41,111 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void hhh15865Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		ParentEntityIdWrapper idWrapper = new ParentEntityIdWrapper(
+				new ParentEntityId(1)
+		);
+		ParentEntity parentEntity = new ParentEntity(
+				idWrapper,
+				List.of(new ChildEntity())
+		);
+
+		entityManager.merge( parentEntity );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity
+	public static class ChildEntity {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+	@Embeddable
+	public static class ParentEntityId {
+		private int id;
+
+		public ParentEntityId() {
+		}
+
+		public ParentEntityId(int id) {
+			this.id = id;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+	@Embeddable
+	public static class ParentEntityIdWrapper implements Serializable {
+		@Embedded
+		private ParentEntityId parentEntityId;
+
+		public ParentEntityIdWrapper() {
+		}
+
+		public ParentEntityIdWrapper(ParentEntityId parentEntityId) {
+			this.parentEntityId = parentEntityId;
+		}
+
+		public ParentEntityId getParentEntityId() {
+			return parentEntityId;
+		}
+
+		public void setParentEntityId(ParentEntityId parentEntityId) {
+			this.parentEntityId = parentEntityId;
+		}
+	}
+
+	@Entity
+	public static class ParentEntity {
+		@EmbeddedId
+		private ParentEntityIdWrapper parentEntityIdWrapper;
+
+		@OneToMany(cascade = CascadeType.ALL)
+		@JoinColumn(name = "parent_entity_id", referencedColumnName = "id")
+		private List<ChildEntity> childEntities;
+
+		public ParentEntity() {
+		}
+
+		public ParentEntity(ParentEntityIdWrapper parentEntityIdWrapper, List<ChildEntity> childEntities) {
+			this.parentEntityIdWrapper = parentEntityIdWrapper;
+			this.childEntities = childEntities;
+		}
+
+		public ParentEntityIdWrapper getParentEntityIdWrapper() {
+			return parentEntityIdWrapper;
+		}
+
+		public void setParentEntityIdWrapper(ParentEntityIdWrapper parentEntityIdWrapper) {
+			this.parentEntityIdWrapper = parentEntityIdWrapper;
+		}
+
+		public List<ChildEntity> getChildEntities() {
+			return childEntities;
+		}
+
+		public void setChildEntities(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
 	}
 }
