1ad1d85e0f2af3cfd27c5ee08bddcfd4837e5ed1
Marco Belladelli <mbellade@redhat.com>
2023-01-02 10:50:48 +0100

    HHH-15866 Add test for issue

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,27 @@
 package org.hibernate.bugs;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Persistence;
+import javax.persistence.Table;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -28,11 +43,94 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void hhh15866Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		ChildEntity childEntity = new ChildEntity();
+
+		ParentEntity parentEntity = new ParentEntity(
+				new ParentEntityId( 1 ),
+				Collections.singletonList( childEntity )
+		);
+
+		entityManager.persist( parentEntity );
+
+		assertEquals( 1, parentEntity.getChildEntities().size() );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "ChildEntity")
+	@Table(name = "child_entity")
+	public static class ChildEntity {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+	@Embeddable
+	public static class ParentEntityId implements Serializable {
+		int id;
+
+		public ParentEntityId() {
+		}
+
+		public ParentEntityId(int id) {
+			this.id = id;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+
+
+	@Entity(name = "ParentEntity")
+	@Table(name = "parent_entity")
+	public static class ParentEntity {
+		@EmbeddedId
+		private ParentEntityId id;
+
+		@OneToMany(cascade = CascadeType.ALL)
+		@JoinColumn(name = "parent_entity_id", referencedColumnName = "id", nullable = false)
+		private List<ChildEntity> childEntities;
+
+		public ParentEntity() {
+		}
+
+		public ParentEntity(ParentEntityId id, List<ChildEntity> childEntities) {
+			this.id = id;
+			this.childEntities = childEntities;
+		}
+
+		public ParentEntityId getId() {
+			return id;
+		}
+
+		public void setId(ParentEntityId id) {
+			this.id = id;
+		}
+
+		public List<ChildEntity> getChildEntities() {
+			return childEntities;
+		}
+
+		public void setChildEntities(List<ChildEntity> childEntities) {
+			this.childEntities = childEntities;
+		}
 	}
 }
