b65840c76e2b47216fb826918673c36a5ebd2382
Marco Belladelli <marco@hibernate.org>
2023-01-17 10:25:04 +0100

    HHH-16023 Add test for issue

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,31 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.Persistence;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -31,8 +50,52 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Session session = entityManager.unwrap( Session.class );
+		List myEntities = session.getNamedNativeQuery( "MyEntity.findMyEntity" )
+				.addEntity( "ame", MyEntity.class )
+				.getResultList();
+		assertNotNull( myEntities );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@NamedNativeQueries(value = {
+			@NamedNativeQuery(
+					name = "MyEntity.findMyEntity",
+					query = "WITH RECURSIVE all_my_entities AS " +
+							"(SELECT me.* FROM my_entity me) " +
+							"SELECT {ame.*} FROM all_my_entities ame")
+	})
+	@Entity
+	@Table(name = "my_entity")
+	@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+	@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, name = "entity_type")
+	public static class MyEntity<E extends MyEntity> {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String name;
+
+	}
+
+	@Entity
+	public static class System {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+	}
+
+	@Entity
+	@DiscriminatorValue("SUPPORTING_ASSET_TYPE")
+	public static class EntityType extends MyEntity<EntityType> {
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "system_id")
+		System system;
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,32 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+
+import org.hibernate.Session;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedNativeQueries;
+import jakarta.persistence.NamedNativeQuery;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Table;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -31,8 +51,52 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Session session = entityManager.unwrap( Session.class );
+		List myEntities = session.getNamedNativeQuery( "MyEntity.findMyEntity" )
+				.addEntity( "ame", MyEntity.class )
+				.getResultList();
+		assertNotNull( myEntities );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@NamedNativeQueries(value = {
+			@NamedNativeQuery(
+					name = "MyEntity.findMyEntity",
+					query = "WITH RECURSIVE all_my_entities AS " +
+							"(SELECT me.* FROM my_entity me) " +
+							"SELECT {ame.*} FROM all_my_entities ame")
+	})
+	@Entity
+	@Table(name = "my_entity")
+	@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+	@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, name = "entity_type")
+	public static class MyEntity<E extends MyEntity> {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String name;
+
+	}
+
+	@Entity
+	public static class System {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+	}
+
+	@Entity
+	@DiscriminatorValue("SUPPORTING_ASSET_TYPE")
+	public static class EntityType extends MyEntity<EntityType> {
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "system_id")
+		System system;
 	}
 }
