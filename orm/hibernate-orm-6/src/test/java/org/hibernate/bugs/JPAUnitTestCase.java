9d86b996732557de3f4cc8b7cc926b389843ada2
Marco Belladelli <marco@hibernate.org>
2023-03-16 13:46:19 +0100

    HHH-16280 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,16 @@
 package org.hibernate.bugs;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import java.time.LocalDate;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -25,14 +29,43 @@ public class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
+	//		private static final ObjectMapper objectMapper = new XmlMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		objectMapper.findAndRegisterModules();
+		objectMapper.configure( SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false );
+
+		LocalDate[] array = new LocalDate[] { LocalDate.now() };
+
+		System.out.println( objectMapper.writeValueAsString( array ) );
+
+		System.out.println( objectMapper.writeValueAsString( LocalDate.now() ) );
+
+		System.out.println( objectMapper.writeValueAsString( new String[] { "", "test", null, "test2" } ) );
+
+		System.out.println( objectMapper.writeValueAsString( new MyEnum[] {
+				MyEnum.FALSE,
+				MyEnum.FALSE,
+				null,
+				MyEnum.TRUE
+		} ) );
+
+
+//		LocalDateTime test1 = LocalDateTime.now();
+//		System.out.println( objectMapper.writeValueAsString( test1 ) );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public enum MyEnum {
+		FALSE, TRUE
 	}
 }
