f97a3d78256c1c9684f3e534806dca5c7ba5f7f8
Marco Belladelli <marco@hibernate.org>
2023-03-21 14:17:12 +0100

    HHH-16285 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,26 @@
 package org.hibernate.bugs;
 
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Persistence;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -18,6 +32,14 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Bar bar = new Bar( "unique3" );
+		entityManager.persist( bar );
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
 	}
 
 	@After
@@ -31,8 +53,79 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Bar reference = entityManager.getReference( Bar.class, 1L );
+		Foo merged = entityManager.merge( new Foo( reference ) );
+		// Assertions.assertThat( merged.getBar().getKey() ).isEqualTo( "unique3" );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
+
+	@Entity( name = "Foo" )
+	static class Foo {
+		Foo(Bar bar) {
+			this.bar = bar;
+		}
+
+		Foo() {
+		}
+
+		@Id
+		@GeneratedValue
+		long id;
+		@ManyToOne( cascade = CascadeType.PERSIST, fetch = FetchType.EAGER )
+		@Fetch( FetchMode.JOIN )
+		@JoinColumn( name = "bar_key", referencedColumnName = "nat_key" )
+		Bar bar;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public Bar getBar() {
+			return bar;
+		}
+
+		public void setBar(Bar bar) {
+			this.bar = bar;
+		}
+	}
+
+	@Entity( name = "Bar" )
+	static class Bar implements Serializable {
+		Bar(String key) {
+			this.key = key;
+		}
+
+		Bar() {
+		}
+
+		@Id
+		@GeneratedValue
+		long id;
+		@Column( name = "nat_key", unique = true )
+		String key;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+	}
+
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,22 @@
 package org.hibernate.bugs;
 
+import java.io.Serializable;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Persistence;
+import org.assertj.core.api.Assertions;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +32,14 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Bar bar = new Bar( "unique3" );
+		entityManager.persist( bar );
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
 	}
 
 	@After
@@ -31,8 +53,78 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Bar reference = entityManager.getReference( Bar.class, 1L );
+		Foo merged = entityManager.merge( new Foo( reference ) );
+		Assertions.assertThat( merged.getBar().getKey() ).isEqualTo( "unique3" );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Foo" )
+	static class Foo {
+		Foo(Bar bar) {
+			this.bar = bar;
+		}
+
+		Foo() {
+		}
+
+		@Id
+		@GeneratedValue
+		long id;
+		@ManyToOne( cascade = CascadeType.PERSIST, fetch = FetchType.EAGER )
+		@Fetch( FetchMode.JOIN )
+		@JoinColumn( name = "bar_key", referencedColumnName = "nat_key" )
+		Bar bar;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public Bar getBar() {
+			return bar;
+		}
+
+		public void setBar(Bar bar) {
+			this.bar = bar;
+		}
+	}
+
+	@Entity( name = "Bar" )
+	static class Bar implements Serializable {
+		Bar(String key) {
+			this.key = key;
+		}
+
+		Bar() {
+		}
+
+		@Id
+		@GeneratedValue
+		long id;
+		@Column( name = "nat_key", unique = true )
+		String key;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
 	}
 }
