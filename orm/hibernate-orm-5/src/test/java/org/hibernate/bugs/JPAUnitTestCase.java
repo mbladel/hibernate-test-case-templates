0405ea98cbe0ea2bd523ae2f26c7af8edacd997e
Marco Belladelli <marco@hibernate.org>
2023-04-06 15:06:15 +0200

    HHH-16392 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,26 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import org.junit.After;
 import org.junit.Before;
@@ -31,8 +49,60 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.unwrap( Session.class ).enableFilter( "filter_1" );
+
+		entityManager.createQuery( "DELETE FROM UserEntity WHERE firstName = 'ciao'" ).executeUpdate();
+		entityManager.createQuery( "DELETE FROM UserEntity" ).executeUpdate();
+
+		entityManager.createQuery( "UPDATE UserEntity SET firstName = 'ciao' where id = 1" ).executeUpdate();
+		entityManager.createQuery( "UPDATE UserEntity SET firstName = 'ciao'" ).executeUpdate();
+
+		entityManager.createQuery( "from UserEntity where id = 1" ).getResultList();
+		entityManager.createQuery( "from UserEntity" ).getResultList();
+
+		entityManager.find( UserEntity.class, 1L );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@MappedSuperclass
+	public static class BaseEntity {
+		@Id
+		@GeneratedValue
+		private Long id;
+	}
+
+	@Entity( name = "UserEntity" )
+	@Table( name = "users" )
+	@SQLDelete( sql = "UPDATE users SET deleted = true WHERE id = ? and version = ?" )
+//	@Where( clause = "deleted = false" )
+	@FilterDef( name="filter_1", defaultCondition = "deleted = false")
+	@Filter( name = "filter_1", condition = "deleted = false")
+	public class UserEntity extends BaseEntity {
+		private String firstName;
+
+		private String lastName;
+
+		@ManyToMany( fetch = FetchType.EAGER )
+		@JoinTable(
+				name = "users_roles",
+				joinColumns = @JoinColumn( name = "user_id", referencedColumnName = "id" ),
+				inverseJoinColumns = @JoinColumn( name = "role_id", referencedColumnName = "id" ) )
+		private List<RoleEntity> roles;
+
+		@Column( insertable = false, updatable = false )
+		private boolean deleted;
+	}
+
+	@Entity
+	@Table( name = "role" )
+	public class RoleEntity extends BaseEntity {
+		@Column( length = 25, unique = true )
+		private String name;
+
+		@ManyToMany( mappedBy = "roles" )
+		private List<UserEntity> users;
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,5 +1,24 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -31,8 +50,56 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery( "DELETE FROM UserEntity WHERE firstName = 'ciao'" ).executeUpdate();
+		entityManager.createQuery( "DELETE FROM UserEntity" ).executeUpdate();
+
+//		entityManager.createQuery( "UPDATE UserEntity SET firstName = 'ciao' where id = 1" ).executeUpdate();
+//		entityManager.createQuery( "UPDATE UserEntity SET firstName = 'ciao'" ).executeUpdate();
+
+//		entityManager.createQuery( "from UserEntity where id = 1" ).getResultList();
+//		entityManager.createQuery( "from UserEntity" ).getResultList();
+
+//		entityManager.find( UserEntity.class, 1L );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@MappedSuperclass
+	public static class BaseEntity {
+		@Id
+		@GeneratedValue
+		private Long id;
+	}
+
+	@Entity( name = "UserEntity" )
+	@Table( name = "users" )
+	@Where( clause = "deleted = false" )
+	@SQLDelete( sql = "UPDATE users SET deleted = true WHERE id = ? and version = ?" )
+	public class UserEntity extends BaseEntity {
+		private String firstName;
+
+		private String lastName;
+
+		@ManyToMany( fetch = FetchType.EAGER )
+		@JoinTable(
+				name = "users_roles",
+				joinColumns = @JoinColumn( name = "user_id", referencedColumnName = "id" ),
+				inverseJoinColumns = @JoinColumn( name = "role_id", referencedColumnName = "id" ) )
+		private List<RoleEntity> roles;
+
+		@Column( insertable = false, updatable = false )
+		private boolean deleted;
+	}
+
+	@Entity
+	@Table( name = "role" )
+	public class RoleEntity extends BaseEntity {
+		@Column( length = 25, unique = true )
+		private String name;
+
+		@ManyToMany( mappedBy = "roles" )
+		private List<UserEntity> users;
 	}
 }
