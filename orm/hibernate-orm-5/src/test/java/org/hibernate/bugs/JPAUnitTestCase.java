4ee4ecf026e55f0cfa5b50b3f1f0b06e7299e7f6
Marco Belladelli <marco@hibernate.org>
2023-04-14 09:44:35 +0200

    HHH-16377 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,17 @@
 package org.hibernate.bugs;
 
+import java.time.LocalTime;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -18,6 +23,16 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		final SimpleEntity e2 = new SimpleEntity( 2, LocalTime.MAX );
+		entityManager.persist( e2 );
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
 	}
 
 	@After
@@ -31,8 +46,31 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final SimpleEntity entity = entityManager.find( SimpleEntity.class, 2 );
+		assertThat( entity.getTheLocalTime() ).isEqualToIgnoringNanos( LocalTime.MAX );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "SimpleEntity" )
+	public static class SimpleEntity {
+		@Id
+		private Integer id;
+
+		private LocalTime theLocalTime;
+
+		public SimpleEntity() {
+		}
+
+		public SimpleEntity(Integer id, LocalTime theLocalTime) {
+			this.id = id;
+			this.theLocalTime = theLocalTime;
+		}
+
+		public LocalTime getTheLocalTime() {
+			return theLocalTime;
+		}
 	}
 }
