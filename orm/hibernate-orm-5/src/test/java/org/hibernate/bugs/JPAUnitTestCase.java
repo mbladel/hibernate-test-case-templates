22fc946db61513081c0e89618066a8b5fb48ca10
Marco Belladelli <marco@hibernate.org>
2023-04-17 09:32:46 +0200

    HHH-16452 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,22 @@
 package org.hibernate.bugs;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.Table;
 
 import org.junit.After;
 import org.junit.Before;
@@ -31,8 +45,37 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Query query = entityManager.createQuery( "select creator.id = :accountId as ownership from Document" );
+		query.setParameter( "accountId", 1L );
+		List results = query.getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Document" )
+	@Table( name = "DOCUMENT" )
+	public class Document {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		protected Long id;
+
+		private String name;
+
+		@ManyToOne( fetch = FetchType.LAZY )
+		@JoinColumn( name = "creator_account_id" )
+		private Account creator;
+	}
+
+	@Entity( name = "Account" )
+	@Table( name = "ACCOUNT" )
+	public class Account {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		protected Long id;
+
+		@Column( nullable = false )
+		protected String name;
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,20 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import jakarta.persistence.Table;
 
 import org.junit.After;
 import org.junit.Before;
@@ -31,8 +43,37 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Query query = entityManager.createQuery( "select creator.id = :accountId as ownership from Document" );
+		query.setParameter( "accountId", 1L );
+		List results = query.getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Document" )
+	@Table( name = "DOCUMENT" )
+	public class Document {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		protected Long id;
+
+		private String name;
+
+		@ManyToOne( fetch = FetchType.LAZY )
+		@JoinColumn( name = "creator_account_id" )
+		private Account creator;
+	}
+
+	@Entity( name = "Account" )
+	@Table( name = "ACCOUNT" )
+	public class Account {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		protected Long id;
+
+		@Column( nullable = false )
+		protected String name;
 	}
 }
