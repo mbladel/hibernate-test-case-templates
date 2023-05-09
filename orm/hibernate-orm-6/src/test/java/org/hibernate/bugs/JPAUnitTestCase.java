0f47e5f63da55027a639005374d70c6a9883f65d
Marco Belladelli <marco@hibernate.org>
2023-05-09 13:08:44 +0200

    Better base-line for testing

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,8 @@
 package org.hibernate.bugs;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import java.util.*;
+import jakarta.persistence.*;
+import org.hibernate.annotations.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +18,11 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 
 	@After
