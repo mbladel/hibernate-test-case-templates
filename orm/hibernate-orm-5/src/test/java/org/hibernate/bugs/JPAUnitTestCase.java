320c2173cba0ffa76339561fc9a88cda086591dc
Marco Belladelli <marco@hibernate.org>
2023-07-05 17:27:14 +0200

    HHH-16860 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,19 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import javax.persistence.*;
-import org.hibernate.annotations.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Persistence;
+import javax.persistence.Table;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,6 +32,18 @@ public class JPAUnitTestCase {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
+		final CustomerComputerSystem customerComputer = new CustomerComputerSystem();
+		entityManager.persist( customerComputer );
+		final CustomerCompany customerCompany = new CustomerCompany( 1L );
+		customerCompany.addComputerSystem( customerComputer );
+		entityManager.persist( customerCompany );
+
+		final DistributorComputerSystem distributorComputer = new DistributorComputerSystem();
+		entityManager.persist( distributorComputer );
+		final DistributorCompany distributorCompany = new DistributorCompany( 2L );
+		distributorCompany.addComputerSystem( distributorComputer );
+		entityManager.persist( distributorCompany );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
@@ -36,8 +59,93 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.find( CustomerCompany.class, 1L ).getComputerSystems().size();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Company" )
+	@Inheritance( strategy = InheritanceType.JOINED )
+	public static abstract class Company {
+		@Id
+		private long id;
+
+		public Company() {
+		}
+
+		public Company(long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity( name = "CustomerCompany" )
+	public static class CustomerCompany extends Company {
+		@OneToMany( mappedBy = "owner" )
+		private List<CustomerComputerSystem> computerSystems = new ArrayList<>();
+
+		public CustomerCompany() {
+		}
+
+		public CustomerCompany(long id) {
+			super( id );
+		}
+
+		public void addComputerSystem(CustomerComputerSystem computerSystem) {
+			computerSystems.add( computerSystem );
+			computerSystem.setOwner( this );
+		}
+
+		public List<CustomerComputerSystem> getComputerSystems() {
+			return computerSystems;
+		}
+	}
+
+	@Entity( name = "DistributorCompany" )
+	public static class DistributorCompany extends Company {
+		@OneToMany( mappedBy = "owner" )
+		private List<DistributorComputerSystem> computerSystems = new ArrayList<>();
+
+		public DistributorCompany() {
+		}
+
+		public DistributorCompany(long id) {
+			super( id );
+		}
+
+		public void addComputerSystem(DistributorComputerSystem computerSystem) {
+			computerSystems.add( computerSystem );
+			computerSystem.setOwner( this );
+		}
+
+		public List<DistributorComputerSystem> getComputerSystems() {
+			return computerSystems;
+		}
+	}
+
+	@Entity( name = "ComputerSystem" )
+	@Table( name = "computer_system" )
+	@Inheritance( strategy = InheritanceType.JOINED )
+	public static abstract class ComputerSystem {
+		@Id
+		@GeneratedValue
+		private long id;
+
+		@ManyToOne
+		@JoinColumn( name = "owner_id" )
+		protected Company owner = null;
+
+		public void setOwner(Company owner) {
+			this.owner = owner;
+		}
+	}
+
+	@Entity( name = "CustomerComputerSystem" )
+	public static class CustomerComputerSystem extends ComputerSystem {
+	}
+
+	@Entity( name = "DistributorComputerSystem" )
+	public static class DistributorComputerSystem extends ComputerSystem {
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,24 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-import org.hibernate.annotations.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Table;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -20,6 +32,19 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		final CustomerComputerSystem customerComputer = new CustomerComputerSystem();
+		entityManager.persist( customerComputer );
+		final CustomerCompany customerCompany = new CustomerCompany( 1L );
+		customerCompany.addComputerSystem( customerComputer );
+		entityManager.persist( customerCompany );
+
+		final DistributorComputerSystem distributorComputer = new DistributorComputerSystem();
+		entityManager.persist( distributorComputer );
+		final DistributorCompany distributorCompany = new DistributorCompany( 2L );
+		distributorCompany.addComputerSystem( distributorComputer );
+		entityManager.persist( distributorCompany );
+
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -36,8 +61,94 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.find( CustomerCompany.class, 1L ).getComputerSystems().size();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+
+	@Entity( name = "Company" )
+	@Inheritance( strategy = InheritanceType.JOINED )
+	public static abstract class Company {
+		@Id
+		private long id;
+
+		public Company() {
+		}
+
+		public Company(long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity( name = "CustomerCompany" )
+	public static class CustomerCompany extends Company {
+		@OneToMany( mappedBy = "owner" )
+		private List<CustomerComputerSystem> computerSystems = new ArrayList<>();
+
+		public CustomerCompany() {
+		}
+
+		public CustomerCompany(long id) {
+			super( id );
+		}
+
+		public void addComputerSystem(CustomerComputerSystem computerSystem) {
+			computerSystems.add( computerSystem );
+			computerSystem.setOwner( this );
+		}
+
+		public List<CustomerComputerSystem> getComputerSystems() {
+			return computerSystems;
+		}
+	}
+
+	@Entity( name = "DistributorCompany" )
+	public static class DistributorCompany extends Company {
+		@OneToMany( mappedBy = "owner" )
+		private List<DistributorComputerSystem> computerSystems = new ArrayList<>();
+
+		public DistributorCompany() {
+		}
+
+		public DistributorCompany(long id) {
+			super( id );
+		}
+
+		public void addComputerSystem(DistributorComputerSystem computerSystem) {
+			computerSystems.add( computerSystem );
+			computerSystem.setOwner( this );
+		}
+
+		public List<DistributorComputerSystem> getComputerSystems() {
+			return computerSystems;
+		}
+	}
+
+	@Entity( name = "ComputerSystem" )
+	@Table( name = "computer_system" )
+	@Inheritance( strategy = InheritanceType.JOINED )
+	public static abstract class ComputerSystem {
+		@Id
+		@GeneratedValue
+		private long id;
+
+		@ManyToOne
+		@JoinColumn( name = "owner_id" )
+		protected Company owner = null;
+
+		public void setOwner(Company owner) {
+			this.owner = owner;
+		}
+	}
+
+	@Entity( name = "CustomerComputerSystem" )
+	public static class CustomerComputerSystem extends ComputerSystem {
+	}
+
+	@Entity( name = "DistributorComputerSystem" )
+	public static class DistributorComputerSystem extends ComputerSystem {
 	}
 }
