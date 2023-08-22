e7239ea85a1384241ed2608ba7c862b2cc3aa52c
Marco Belladelli <marco@hibernate.org>
2023-08-22 10:35:52 +0200

    HHH-17045 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -3,6 +3,8 @@ package org.hibernate.bugs;
 import java.util.*;
 import javax.persistence.*;
 
+import org.hibernate.annotations.RowId;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,14 +31,109 @@ public class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void errorDuringUpdatingSameEntityFieldInSameTransaction() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		SimpleTestEntity testEntity = new SimpleTestEntity( 1L, "TestStatus" );
+		entityManager.persist( testEntity );
+		entityManager.flush();
+//		entityManager.getTransaction().commit();
+		//Second transaction
+//		entityManager.getTransaction().begin();
+		testEntity.setStatus( "NewStatus" );
+		entityManager.flush();
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Test
+	public void errorDuringUpdatingRelatedEntityFieldInDifferentTransactions() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		UUID testId = UUID.randomUUID();
+		RelatedMainEntity testEntity = new RelatedMainEntity( testId, new RelatedSecondaryEntity( testId, "OldStatus" ) );
+		//First transaction
+		entityManager.getTransaction().begin();
+		entityManager.persist( testEntity );
+		entityManager.flush();
+//		entityManager.getTransaction().commit();
+		//Second transaction
+//		entityManager.getTransaction().begin();
+		RelatedMainEntity savedEntity = entityManager.find( RelatedMainEntity.class, testId );
+		savedEntity.setStatus( "NewStatus" );
+		entityManager.flush();
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+}
+
+@Entity
+@RowId( "_rowid_" )
+class SimpleTestEntity {
+	@Id
+	@Column( name = "id" )
+	public Long id;
+	@Column( name = "status" )
+	public String status;
+
+	public SimpleTestEntity() {
+	}
+
+	public SimpleTestEntity(Long id, String status) {
+		this.id = id;
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
+
+@Entity
+@RowId( "_rowid_" )
+class RelatedMainEntity {
+	@Id
+	@Column( name = "id" )
+	public UUID id;
+	@OneToOne( cascade = { CascadeType.ALL } )
+	@MapsId
+	public RelatedSecondaryEntity secondary;
+
+	public RelatedMainEntity() {
+	}
+
+	public RelatedMainEntity(UUID id, RelatedSecondaryEntity secondary) {
+		this.id = id;
+		this.secondary = secondary;
+	}
+
+	public String getStatus() {
+		return secondary.status;
+	}
+
+	public void setStatus(String newStatus) {
+		secondary.status = newStatus;
+	}
+}
+
+@Entity
+@RowId( "_rowid_" )
+class RelatedSecondaryEntity {
+	@Id
+	@Column( name = "id" )
+	public UUID id;
+	@Column( name = "status" )
+	public String status;
+
+	public RelatedSecondaryEntity() {
+	}
+
+	public RelatedSecondaryEntity(UUID id, String status) {
+		this.id = id;
+		this.status = status;
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,27 +1,27 @@
 package org.hibernate.bugs;
 
-import java.util.*;
 import jakarta.persistence.*;
+
+import org.hibernate.annotations.RowId;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.UUID;
+
+import static jakarta.persistence.CascadeType.*;
+import static jakarta.persistence.CascadeType.DETACH;
+
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
 public class JPAUnitTestCase {
-
 	private EntityManagerFactory entityManagerFactory;
 
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-
-		entityManager.getTransaction().commit();
-		entityManager.close();
 	}
 
 	@After
@@ -32,11 +32,108 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void errorDuringUpdatingSameEntityFieldInSameTransaction() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		var testEntity = new SimpleTestEntity( 1L, "TestStatus" );
+		entityManager.persist( testEntity );
+		entityManager.flush();
+//		entityManager.getTransaction().commit();
+		//Second transaction
+//		entityManager.getTransaction().begin();
+		testEntity.setStatus( "NewStatus" );
+		entityManager.flush();
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Test
+	public void errorDuringUpdatingRelatedEntityFieldInDifferentTransactions() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		var testId = UUID.randomUUID();
+		var testEntity = new RelatedMainEntity( testId, new RelatedSecondaryEntity( testId, "OldStatus" ) );
+		//First transaction
+		entityManager.getTransaction().begin();
+		entityManager.persist( testEntity );
+		entityManager.flush();
+//		entityManager.getTransaction().commit();
+		//Second transaction
+//		entityManager.getTransaction().begin();
+		var savedEntity = entityManager.find( RelatedMainEntity.class, testId );
+		savedEntity.setStatus( "NewStatus" );
+		entityManager.flush();
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+}
+
+@Entity
+@RowId
+class SimpleTestEntity {
+	@Id
+	@Column( name = "id" )
+	public Long id;
+	@Column( name = "status" )
+	public String status;
+
+	public SimpleTestEntity() {
+	}
+
+	public SimpleTestEntity(Long id, String status) {
+		this.id = id;
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
+
+@Entity
+@RowId
+class RelatedMainEntity {
+	@Id
+	@Column( name = "id" )
+	public UUID id;
+	@OneToOne( cascade = { PERSIST, MERGE, REFRESH, DETACH } )
+	@MapsId
+	public RelatedSecondaryEntity secondary;
+
+	public RelatedMainEntity() {
+	}
+
+	public RelatedMainEntity(UUID id, RelatedSecondaryEntity secondary) {
+		this.id = id;
+		this.secondary = secondary;
+	}
+
+	public String getStatus() {
+		return secondary.status;
+	}
+
+	public void setStatus(String newStatus) {
+		secondary.status = newStatus;
+	}
+}
+
+@Entity
+@RowId
+class RelatedSecondaryEntity {
+	@Id
+	@Column( name = "id" )
+	public UUID id;
+	@Column( name = "status" )
+	public String status;
+
+	public RelatedSecondaryEntity() {
+	}
+
+	public RelatedSecondaryEntity(UUID id, String status) {
+		this.id = id;
+		this.status = status;
 	}
 }
