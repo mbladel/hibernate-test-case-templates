f07f4ae62ea863ce6acda5f755434b1c99a34e0a
Marco Belladelli <marco@hibernate.org>
2023-09-04 14:24:50 +0200

    HHH-17155 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,15 @@
 package org.hibernate.bugs;
 
 import java.util.*;
+
 import jakarta.persistence.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -19,6 +23,8 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		entityManager.persist( new TestEntity( "test" ) );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -35,8 +41,87 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final Query query = entityManager.createQuery( String.format(
+				"select new %s(t.id, t.name) from TestEntity t",
+				TestDto.class.getName()
+		), TestDto.class );
+
+		final List<TestDto> resultList = query.getResultList();
+		assertThat( resultList ).hasSize( 1 );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "TestEntity" )
+	public static class TestEntity {
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String name;
+
+		public TestEntity() {
+		}
+
+		public TestEntity(String name) {
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+
+	public static class TestDto {
+		private String value;
+		private String title;
+
+		public TestDto() {
+		}
+
+		public TestDto(String value, String title) {
+			super();
+			this.value = value;
+			this.title = title;
+		}
+
+		public TestDto(long value, String title) {
+			this( String.valueOf( value ), title );
+		}
+
+		public TestDto(int value, String title) {
+			this( String.valueOf( value ), title );
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
 	}
 }
