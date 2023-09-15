c09e9fcae009ec781627d00712be4830f240331c
Marco Belladelli <marco@hibernate.org>
2023-09-15 13:12:13 +0200

    HHH-172108 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,23 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Persistence;
+
+import org.hibernate.annotations.Any;
+import org.hibernate.annotations.AnyMetaDef;
+import org.hibernate.annotations.MetaValue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +47,95 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		final DocClientEntity docClientEntity = new DocClientEntity( 1L, "test_client" );
+		entityManager.persist( docClientEntity );
+		entityManager.flush();
+
+		final DocumentEntity documentEntity = new DocumentEntity( "test_doc" );
+		documentEntity.setParent( docClientEntity );
+		entityManager.persist( documentEntity );
+		entityManager.flush();
+
+		final int count = entityManager.createQuery(
+				"insert into DocumentEntity(name, parent) select name, parent from DocumentEntity" ).executeUpdate();
+
+		assertThat( count ).isEqualTo( 1 );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public interface IDocumentEntity {
+		Long getId();
+
+		String getName();
+	}
+
+	@Entity( name = "DocumentEntity" )
+	public static class DocumentEntity implements IDocumentEntity {
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String name;
+
+		@JoinColumn( name = "parent_id" )
+		@Any( metaColumn = @Column( name = "parent_type" ) )
+		@AnyMetaDef( metaType = "string", idType = "long", metaValues = {
+				@MetaValue( value = "document", targetEntity = DocumentEntity.class ),
+				@MetaValue( value = "doc_client", targetEntity = DocClientEntity.class ),
+		} )
+		private IDocumentEntity parent;
+
+		public DocumentEntity() {
+		}
+
+		public DocumentEntity(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		public IDocumentEntity getParent() {
+			return parent;
+		}
+
+		public void setParent(IDocumentEntity parent) {
+			this.parent = parent;
+		}
+	}
+
+	@Entity( name = "DocClientEntity" )
+	public static class DocClientEntity implements IDocumentEntity {
+		@Id
+		private Long id;
+
+		private String name;
+
+		public DocClientEntity() {
+		}
+
+		public DocClientEntity(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,24 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import org.hibernate.annotations.Any;
+import org.hibernate.annotations.AnyDiscriminatorValue;
+import org.hibernate.annotations.AnyKeyJavaType;
+import org.hibernate.type.descriptor.java.LongJavaType;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +48,97 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final DocClientEntity docClientEntity = new DocClientEntity( 1L, "test_client" );
+		entityManager.persist( docClientEntity );
+		entityManager.flush();
+
+		final DocumentEntity documentEntity = new DocumentEntity( 2L, "test_doc" );
+		documentEntity.setParent( docClientEntity );
+		entityManager.persist( documentEntity );
+		entityManager.flush();
+
+		final int count = entityManager.createQuery(
+				"insert into DocumentEntity(name, parent) select name, parent from DocumentEntity"
+		).executeUpdate();
+
+		assertThat( count ).isEqualTo( 1 );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public interface IDocumentEntity {
+		Long getId();
+
+		String getName();
+	}
+
+	@Entity( name = "DocumentEntity" )
+	public static class DocumentEntity implements IDocumentEntity {
+		@Id
+		private Long id;
+
+		private String name;
+
+		@Any( fetch = FetchType.LAZY )
+		@JoinColumn( name = "parent_id" )
+		@Column( name = "parent_type" )
+		@AnyKeyJavaType( value = LongJavaType.class )
+		@AnyDiscriminatorValue( discriminator = "document", entity = DocumentEntity.class )
+		@AnyDiscriminatorValue( discriminator = "doc_client", entity = DocClientEntity.class )
+		private IDocumentEntity parent;
+
+		public DocumentEntity() {
+		}
+
+		public DocumentEntity(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		public IDocumentEntity getParent() {
+			return parent;
+		}
+
+		public void setParent(IDocumentEntity parent) {
+			this.parent = parent;
+		}
+	}
+
+	@Entity( name = "DocClientEntity" )
+	public static class DocClientEntity implements IDocumentEntity {
+		@Id
+		private Long id;
+
+		private String name;
+
+		public DocClientEntity() {
+		}
+
+		public DocClientEntity(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
 	}
 }
