207884dd9403323aafd675125a2e6dd36d2e0fd5
Marco Belladelli <marco@hibernate.org>
2023-09-15 12:55:34 +0200

    HHH-17211 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,30 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import org.hibernate.annotations.Any;
+import org.hibernate.annotations.AnyDiscriminatorValue;
+import org.hibernate.annotations.AnyKeyJavaType;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.type.descriptor.java.LongJavaType;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import jakarta.persistence.Table;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +54,113 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final DocClientEntity docClientEntity = new DocClientEntity();
+		docClientEntity.setName( "test doc client" );
+		entityManager.persist( docClientEntity );
+		entityManager.flush();
+
+		final DocumentEntity documentEntity = new DocumentEntity();
+		documentEntity.setName( "test doc" );
+		documentEntity.setParent( docClientEntity );
+		entityManager.persist( documentEntity );
+		entityManager.flush();
+
+		Query query = entityManager.createQuery( " select id, name, parent from DocumentEntity " );
+
+		query.getResultList();
+
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public interface IDocumentEntity {
+		Long getId();
+
+		String getName();
+	}
+
+	@Entity( name = "DocumentEntity" )
+	@Table( name = "`DOCUMENT`" )
+	@DynamicUpdate()
+	public static class DocumentEntity implements IDocumentEntity {
+
+		@Id
+		@Column( name = "`ID`", nullable = false )
+		@Access( AccessType.PROPERTY )
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		private Long id;
+
+		@Column( name = "`NAME`", nullable = false )
+		private String name;
+
+		@Any( fetch = FetchType.LAZY )
+		@JoinColumn( name = "`PARENTID`" )
+		@Column( name = "PARENTTABLE" )
+		@AnyKeyJavaType( value = LongJavaType.class )
+		@AnyDiscriminatorValue( discriminator = "DOCUMENT", entity = DocumentEntity.class )
+		@AnyDiscriminatorValue( discriminator = "DOCCLIENT", entity = DocClientEntity.class )
+		private IDocumentEntity parent;
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public IDocumentEntity getParent() {
+			return parent;
+		}
+
+		public void setParent(IDocumentEntity parent) {
+			this.parent = parent;
+		}
+	}
+
+	@Entity( name = "DocClientEntity" )
+	@Table( name = "`DOCCLIENT`" )
+	@DynamicUpdate()
+	public static class DocClientEntity implements IDocumentEntity {
+
+		@Id
+		@Column( name = "`ID`", nullable = false )
+		@Access( AccessType.PROPERTY )
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		private Long id;
+
+		@Column( name = "`NAME`", nullable = false )
+		private String name;
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
 	}
 }
