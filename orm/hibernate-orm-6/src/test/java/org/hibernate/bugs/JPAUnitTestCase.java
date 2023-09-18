9b8c465b6d08890c1e7a3ea149861204b0abe85f
Marco Belladelli <marco@hibernate.org>
2023-09-18 15:15:31 +0200

    HHH-17205 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,20 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.io.Serializable;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -19,6 +28,11 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		final ParentEntity parentEntity = new ParentEntity( "1", "one" );
+		final ChildEntity childEntity = new ChildEntity( "1", "one" );
+		entityManager.persist( parentEntity );
+		entityManager.persist( childEntity );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -35,8 +49,86 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		var query = entityManager.createQuery(
+				"select p from ParentEntity p where p.first = :first",
+				ParentEntity.class
+		);
+		query.setParameter( "first", "1" );
+
+		var result = query.getSingleResult();
+		System.out.println( result );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public static class CompositeId implements Serializable {
+		private String first;
+		private String second;
+	}
+
+	@Entity( name = "ParentEntity" )
+	@IdClass( CompositeId.class )
+	public static class ParentEntity {
+		@Id
+		@Column( name = "col1" )
+		private String first;
+
+		@Id
+		@Column( name = "col2" )
+		private String second;
+
+		@OneToOne( mappedBy = "parentEntity" )
+		private ChildEntity childEntity;
+
+		public ParentEntity() {
+		}
+
+		public ParentEntity(String first, String second) {
+			this.first = first;
+			this.second = second;
+		}
+
+		public ChildEntity getChildEntity() {
+			return childEntity;
+		}
+
+		public void setChildEntity(ChildEntity childEntity) {
+			this.childEntity = childEntity;
+		}
+	}
+
+	@Entity( name = "ChildEntity" )
+	@IdClass( CompositeId.class )
+	public static class ChildEntity {
+		@Id
+		@Column( name = "col1" )
+		private String first;
+
+		@Id
+		@Column( name = "col2" )
+		private String second;
+
+		@OneToOne
+		@JoinColumn( name = "col1", referencedColumnName = "col1" )
+		@JoinColumn( name = "col2", referencedColumnName = "col2" )
+		private ParentEntity parentEntity;
+
+		public ChildEntity() {
+		}
+
+		public ChildEntity(String first, String second) {
+			this.first = first;
+			this.second = second;
+		}
+
+		public ParentEntity getParentEntity() {
+			return parentEntity;
+		}
+
+		public void setParentEntity(ParentEntity parentEntity) {
+			this.parentEntity = parentEntity;
+		}
 	}
 }
