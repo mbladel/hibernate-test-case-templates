a68d2d2ef1603bf7f42c51e997b2973f55713730
Marco Belladelli <marco@hibernate.org>
2023-10-05 11:33:23 +0200

    HHH-17193 tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,14 @@
 package org.hibernate.bugs;
 
+import java.io.Serializable;
 import java.util.*;
 import javax.persistence.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -19,6 +22,15 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		final SomeEntity someEntity = new SomeEntity( 1L );
+		final SomeChildEntity child1 = new SomeChildEntity( 1L, someEntity );
+		final SomeChildEntity child2 = new SomeChildEntity( 2L, someEntity );
+//			someEntity.getChildEntities().addAll( List.of( child1, child2 ) );
+		entityManager.persist( child1 );
+		entityManager.persist( child2 );
+		entityManager.persist( someEntity );
+		entityManager.persist( new CompositePkEntity( new CompositePk( someEntity, "value" ), "initial-value" ) );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -35,8 +47,93 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final SomeEntity someEntity = entityManager.find( SomeEntity.class, 1L );
+//		assertThat( someEntity.getChildEntities() ).hasSize( 2 );
+		final CompositePkEntity compositePkEntity = new CompositePkEntity(
+				new CompositePk( someEntity, "value" ),
+				"new-value"
+		);
+		entityManager.merge( compositePkEntity );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "SomeChildEntity" )
+	public static class SomeChildEntity {
+		@Id
+		private Long id;
+
+		@ManyToOne
+		@JoinColumn( name = "some_entity" )
+		private SomeEntity someEntity;
+
+		public SomeChildEntity() {
+		}
+
+		public SomeChildEntity(Long id, SomeEntity someEntity) {
+			this.id = id;
+			this.someEntity = someEntity;
+		}
+	}
+
+	@Entity( name = "SomeEntity" )
+	public static class SomeEntity {
+		@Id
+		private Long id;
+
+		@OneToMany( mappedBy = "someEntity", cascade = CascadeType.ALL )
+		private List<SomeChildEntity> childEntities = new ArrayList<>();
+
+		public SomeEntity() {
+		}
+
+		public SomeEntity(Long id) {
+			this.id = id;
+		}
+
+		public List<SomeChildEntity> getChildEntities() {
+			return childEntities;
+		}
+	}
+
+	@Embeddable
+	public static class CompositePk implements Serializable {
+		@ManyToOne(cascade = CascadeType.ALL)
+		@JoinColumn( name = "some_entity_id", nullable = false )
+		private SomeEntity someEntity;
+
+		@Column( name = "some_pk_value", nullable = false )
+		private String somePkValue;
+
+		public CompositePk() {
+		}
+
+		public CompositePk(SomeEntity someEntity, String somePkValue) {
+			this.someEntity = someEntity;
+			this.somePkValue = somePkValue;
+		}
+	}
+
+	@Entity( name = "CompositePkEntity" )
+	public static class CompositePkEntity {
+		@EmbeddedId
+		private CompositePk id;
+
+		@Column( name = "some_value" )
+		private String someValue;
+
+		public CompositePkEntity() {
+		}
+
+		public CompositePkEntity(CompositePk id, String someValue) {
+			this.id = id;
+			this.someValue = someValue;
+		}
+
+		public String getSomeValue() {
+			return someValue;
+		}
 	}
 }
