044d481756d8505a4504a3e5aaf967f93f934cb8
Marco Belladelli <marco@hibernate.org>
2023-10-10 15:01:21 +0200

    HHH-17144 test

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,17 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.sql.Blob;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +41,53 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		var list = entityManager.createQuery( "select distinct a from TestData a order by a.expTimest", TestData.class )
+				.setFirstResult( 0 ).setMaxResults( 2 ).getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "TestData" )
+	public static class TestData {
+		@Id
+		private Long id;
+		private Integer type;
+		private String expTimest;
+		@Lob
+		private Blob data;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Integer getType() {
+			return type;
+		}
+
+		public void setType(Integer type) {
+			this.type = type;
+		}
+
+		public String getExpTimest() {
+			return expTimest;
+		}
+
+		public void setExpTimest(String expTimest) {
+			this.expTimest = expTimest;
+		}
+
+		public Blob getData() {
+			return data;
+		}
+
+		public void setData(Blob data) {
+			this.data = data;
+		}
 	}
 }
