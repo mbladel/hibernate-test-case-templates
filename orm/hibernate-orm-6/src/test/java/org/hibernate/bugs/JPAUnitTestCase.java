414001af908642645831d92c21e6bf16c6853652
Marco Belladelli <marco@hibernate.org>
2023-11-17 12:22:15 +0100

    HHH-17405 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,22 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +46,94 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery( "select c from ChildA c where c.parent.id = 1" ).getFirstResult();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@MappedSuperclass
+	public static abstract class Child<P extends Parent<?>> {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		private Long id;
+		@ManyToOne( optional = false )
+		private P parent;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public P getParent() {
+			return parent;
+		}
+
+		public void setParent(P parent) {
+			this.parent = parent;
+		}
+	}
+
+	@MappedSuperclass
+	public static abstract class Parent<C extends Child<?>> {
+		@OneToMany( mappedBy = "parent" )
+		private List<C> children = new LinkedList<>();
+
+		public abstract Long getId();
+
+		public abstract void setId(Long id);
+
+		public List<C> getChildren() {
+			return children;
+		}
+
+		public void setChildren(List<C> children) {
+			this.children = children;
+		}
+	}
+
+	@Entity( name = "ChildA" )
+	public static class ChildA extends Child<ParentA> {
+	}
+
+	@Entity( name = "ChildB" )
+	public static class ChildB extends Child<ParentB> {
+	}
+
+	@Entity( name = "ParentA" )
+	public static class ParentA extends Parent<ChildA> {
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity( name = "ParentB" )
+	public static class ParentB extends Parent<ChildB> {
+		@Id
+		private Long id;
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public void setId(Long id) {
+			this.id = id;
+		}
 	}
 }
