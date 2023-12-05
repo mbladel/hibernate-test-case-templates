9672b9faa52865ed2978673dc6c98244a32d7afc
Marco Belladelli <marco@hibernate.org>
2023-12-05 11:21:48 +0100

    HHH-17515 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,20 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import jakarta.persistence.Table;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -19,6 +28,8 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		entityManager.persist( new Product( (short) 1, "product_1" ) );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -35,8 +46,92 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		Query query = entityManager.createQuery( String.format(
+				"select new %s(product.productNo,  product.name) from Product product",
+				KeyValue.class.getName()
+		) );
+
+		final List resultList = query.getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Product" )
+	@Table( name = "t_product_main" )
+	public static class Product {
+		@Id
+		@GeneratedValue
+		private Integer uniqid;
+
+		private Short productNo;
+
+		private String name;
+
+		public Product() {
+		}
+
+		public Product(Short productNo, String name) {
+			this.productNo = productNo;
+			this.name = name;
+		}
+	}
+
+	public static class KeyValue {
+		private Integer key;
+
+		private String value;
+
+		public KeyValue() {
+		}
+
+		public KeyValue(Short k) {
+			System.out.println( "inside 1" );
+			key = k == null ? null : k.intValue();
+		}
+
+		public KeyValue(Integer k, String val) {
+			key = k;
+			value = val;
+		}
+
+		public KeyValue(Short k, String val) {
+			System.out.println( "inside 2" );
+			key = k == null ? null : k.intValue();
+			value = val;
+		}
+
+		public KeyValue(Short k, Short val) {
+			key = k == null ? null : k.intValue();
+			value = String.valueOf( val );
+		}
+
+		public KeyValue(Integer k, Short val) {
+			key = k == null ? null : k.intValue();
+			value = String.valueOf( val );
+		}
+
+		public KeyValue(Short k, Integer val) {
+			key = k == null ? null : k.intValue();
+			value = String.valueOf( val );
+		}
+
+		public KeyValue(Integer k, Integer val) {
+			key = k == null ? null : k.intValue();
+			value = String.valueOf( val );
+		}
+
+		public KeyValue(Short k, BigDecimal val) {
+			key = k == null ? null : k.intValue();
+			value = String.valueOf( val.setScale( 2, BigDecimal.ROUND_HALF_DOWN ) );
+		}
+
+		public KeyValue(Short k, Double val, Integer convertType) {
+			key = k == null ? null : k.intValue();
+			if ( convertType == 1 ) {
+				value = String.format( "%10.0f", val );
+			}
+		}
 	}
 }
