617402f452b0874877cb8432d3dae14b94c424be
Marco Belladelli <marco@hibernate.org>
2023-12-06 14:35:03 +0100

    HHH-17523 tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,17 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -29,14 +35,41 @@ public class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		entityManager.createQuery( "select c from JPAUnitTestCase$Car c where c.field = 'Some'" ).getFirstResult();
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
+
+	@MappedSuperclass
+	public static abstract class Vehicle<T> {
+		@Id
+		@GeneratedValue
+		private Long id;
+		private T field;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public T getField() {
+			return field;
+		}
+
+		public void setField(T field) {
+			this.field = field;
+		}
+	}
+
+	@Entity
+	public static class Car extends Vehicle<String> {
+	}
+
 }
