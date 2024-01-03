84ff88282b5f3b8d9381a249e097b46d718ca407
Marco Belladelli <marco@hibernate.org>
2024-01-03 15:25:29 +0100

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,22 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.math.BigInteger;
+import java.util.List;
+
+import org.hibernate.Session;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -35,8 +46,29 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final List<Parent> resultList = entityManager.unwrap( Session.class ).createQuery(
+				"from Parent p where element(p.children).id in (:ids)",
+				Parent.class
+		).setParameterList( "ids", List.of( BigInteger.valueOf( 1L ), BigInteger.valueOf( 2L ) ) ).getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Parent" )
+	public static class Parent {
+		@Id
+		private Long id;
+
+		@OneToMany
+		@JoinColumn
+		private List<Child> children;
+	}
+
+	@Entity( name = "Child" )
+	public static class Child {
+		@Id
+		private BigInteger id;
 	}
 }
