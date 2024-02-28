8c05d153ffe9a5a0a16adfd12b187b51845ce97e
Marco Belladelli <marco@hibernate.org>
2024-02-28 09:10:56 +0100

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,26 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaCteCriteria;
+import org.hibernate.query.criteria.JpaRoot;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Tuple;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -17,7 +32,7 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
 		entityManager.getTransaction().commit();
@@ -33,10 +48,63 @@ public class JPAUnitTestCase {
 	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+		// Build main query
+		final JpaCriteriaQuery<ClassA> mainQuery = (JpaCriteriaQuery<ClassA>) cb.createQuery( ClassA.class );
+		final JpaRoot<ClassA> mainRoot = mainQuery.from( ClassA.class );
+
+		final List<Predicate> joinWherePredicates = new ArrayList<>();
+
+		// First criteria
+		final CriteriaQuery<Tuple> firstQuery = cb.createTupleQuery();
+		final Root<ClassA> firstRoot = firstQuery.from( ClassA.class );
+		firstQuery.multiselect( firstRoot.get( "id" ).alias( "id_for_first" ) );
+		firstQuery.where( cb.and(
+				cb.isNotNull( firstRoot.get( "price" ) ),
+				cb.greaterThan( firstRoot.get( "price" ), 1.2d )
+		) );
+		final JpaCteCriteria<Tuple> firstWithAlias = mainQuery.with( firstQuery );
+		final JpaRoot<Tuple> fromFirstQuery = mainQuery.from( firstWithAlias );
+
+		// Second criteria
+		final CriteriaQuery<Tuple> secondQuery = cb.createTupleQuery();
+		final Root<ClassA> secondRoot = secondQuery.from( ClassA.class );
+		secondQuery.multiselect( secondRoot.get( "id" ).alias( "id_for_second" ) );
+		secondQuery.where( cb.and(
+				cb.isNotNull( secondRoot.get( "price" ) ),
+				cb.lessThan( secondRoot.get( "price" ), 150.0d )
+		) );
+		final JpaCteCriteria<Tuple> secondWithAlias = mainQuery.with( secondQuery );
+		final JpaRoot<Tuple> fromSecondQuery = mainQuery.from( secondWithAlias );
+
+		// Join everything
+		joinWherePredicates
+				.add( cb.equal( mainRoot.get( "id" ), fromFirstQuery.get( "id_for_first" ) ) );
+		joinWherePredicates
+				.add( cb.equal( mainRoot.get( "id" ), fromSecondQuery.get( "id_for_second" ) ) );
+
+		mainQuery.select( mainRoot );
+		mainQuery.where( cb.and( joinWherePredicates.toArray( new Predicate[0] ) ) );
+		mainQuery.orderBy( cb.asc( mainRoot.get( "name" ) ) );
+		mainQuery.fetch( 10 );
+
+		final List<ClassA> resultList = entityManager.createQuery( mainQuery )/*.setMaxResults( 10 )*/.getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "ClassA" )
+	public static class ClassA {
+		@Id
+		private Long id;
+
+		private String name;
+
+		private double price;
 	}
 }
