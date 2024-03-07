c0addc78776cc13090395004ccd4642a30e4dfbc
Marco Belladelli <marco@hibernate.org>
2024-03-07 09:24:52 +0100

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,6 +1,15 @@
 package org.hibernate.bugs;
 
 import java.util.*;
+
+import org.hibernate.Session;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SqlFragmentAlias;
+
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+
 import jakarta.persistence.*;
 import jakarta.persistence.criteria.*;
 
@@ -33,11 +42,52 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void testUpdateQuery() {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final Session session = entityManager.unwrap( Session.class );
+		session.enableFilter( "deleted_filter" ).setParameter( "deleted_name", "deleted" );
+		session.createMutationQuery( "update UserEntity set name = 'updated'" ).executeUpdate();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "UserEntity" )
+	@FilterDef( name = "deleted_filter", parameters = @ParamDef( name = "deleted_name", type = String.class ) )
+	@Filter(
+			name = "deleted_filter",
+			condition = "( ({o}.deleted = true and {o}.name = :deleted_name) or ( {o}.deleted = false and exists(select u.id from \"user\" u WHERE u.id = {o}.id and u.name = {o}.name )  ) )",
+			aliases = @SqlFragmentAlias( alias = "o", table = "\"user\"" ),
+			deduceAliasInjectionPoints = false
+	)
+	@Table( name = "\"user\"" )
+	public static class UserEntity {
+		@Id
+		private Long id;
+
+		@Column( name = "deleted" )
+		private boolean deleted;
+
+		@Column( name = "name" )
+		private String name;
+
+		public UserEntity() {
+		}
+
+		public UserEntity(Long id, boolean deleted, String name) {
+			this.id = id;
+			this.deleted = deleted;
+			this.name = name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
 	}
 }
