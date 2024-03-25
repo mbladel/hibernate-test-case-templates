7ddec64b0981494cd7139f63db5e8930a71b4d3c
Marco Belladelli <marco@hibernate.org>
2024-03-25 10:19:59 +0100

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,24 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
+import java.io.Serializable;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -18,8 +30,21 @@ public class JPAUnitTestCase {
 	@Before
 	public void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		final SalesHeaderKey primaryKey = new SalesHeaderKey();
+		primaryKey.setDocumentType( 1 );
+		primaryKey.setNo( "ME-24000029" );
+		final SalesHeader header = new SalesHeader( primaryKey );
+		entityManager.persist( header );
+
+		final SalesHeaderExtensionKey extensionKey = new SalesHeaderExtensionKey();
+		extensionKey.setDocumentType( 1 );
+		extensionKey.setNo( "ME-24000029" );
+		final SalesHeaderExtension extension = new SalesHeaderExtension( extensionKey, header );
+//		final SalesHeaderExtension extension = new SalesHeaderExtension( 1L, header );
+		entityManager.persist( extension );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -34,10 +59,122 @@ public class JPAUnitTestCase {
 	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final SalesHeaderKey primaryKey = new SalesHeaderKey();
+		primaryKey.setDocumentType( 1 );
+		primaryKey.setNo( "ME-24000029" );
+		final SalesHeader header = entityManager.find( SalesHeader.class, primaryKey );
+
+		final SalesHeaderExtension extension = header.getExtension();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "SalesHeader" )
+	static class SalesHeader {
+		@EmbeddedId
+		private SalesHeaderKey key;
+
+		@OneToOne( mappedBy = "header", fetch = FetchType.LAZY )
+		private SalesHeaderExtension extension;
+
+		public SalesHeader() {
+		}
+
+		public SalesHeader(SalesHeaderKey key) {
+			this.key = key;
+			this.extension = extension;
+		}
+
+		public SalesHeaderExtension getExtension() {
+			return extension;
+		}
+	}
+
+	@Embeddable
+	static class SalesHeaderKey implements Serializable {
+		@Basic( optional = false )
+		@Column( name = "doc_type", nullable = false )
+		private int documentType;
+
+		@Basic( optional = false )
+		@Column( name = "No_", length = 20, nullable = false )
+		private String no;
+
+		public int getDocumentType() {
+			return documentType;
+		}
+
+		public void setDocumentType(int documentType) {
+			this.documentType = documentType;
+		}
+
+		public String getNo() {
+			return no;
+		}
+
+		public void setNo(String no) {
+			this.no = no;
+		}
+	}
+
+	@Entity( name = "SalesHeaderExtension" )
+	static class SalesHeaderExtension {
+		@EmbeddedId
+		private SalesHeaderExtensionKey key;
+//		@Id
+//		private Long id;
+
+		@OneToOne( optional = false, fetch = FetchType.LAZY )
+		@JoinColumns( {
+				@JoinColumn( name = "doc_type", referencedColumnName = "doc_type", nullable = false,
+						insertable = false, updatable = false ),
+				@JoinColumn( name = "No_", referencedColumnName = "No_", nullable = false,
+						insertable = false, updatable = false )
+		} )
+		private SalesHeader header;
+
+		public SalesHeaderExtension() {
+		}
+
+		public SalesHeaderExtension(SalesHeaderExtensionKey key, SalesHeader header) {
+			this.key = key;
+			this.header = header;
+		}
+
+//		public SalesHeaderExtension(Long id, SalesHeader header) {
+//			this.id = id;
+//			this.header = header;
+//		}
+	}
+
+	@Embeddable
+	static class SalesHeaderExtensionKey implements Serializable {
+		@Basic( optional = false )
+		@Column( name = "doc_type", nullable = false )
+		private int documentType;
+
+		@Basic( optional = false )
+		@Column( name = "No_", length = 20, nullable = false )
+		private String no;
+
+		public int getDocumentType() {
+			return documentType;
+		}
+
+		public void setDocumentType(int documentType) {
+			this.documentType = documentType;
+		}
+
+		public String getNo() {
+			return no;
+		}
+
+		public void setNo(String no) {
+			this.no = no;
+		}
 	}
 }
