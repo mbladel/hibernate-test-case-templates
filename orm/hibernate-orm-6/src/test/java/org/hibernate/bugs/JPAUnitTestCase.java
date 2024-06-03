272524b733d5327e8a3331b66c6b1b6e47b56d5c
Marco Belladelli <marcobladel@gmail.com>
2024-06-03 14:59:53 +0200

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -8,6 +8,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
@@ -20,6 +22,25 @@ public class JPAUnitTestCase {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		final ContainingEntity entity1 = new ContainingEntity();
+		entity1.setId( 1 );
+
+		final ContainingEntity containingEntity1 = new ContainingEntity();
+		containingEntity1.setId( 2 );
+
+		entity1.setChild( containingEntity1 );
+		containingEntity1.setParent( entity1 );
+
+		final ContainedEntity containedEntity = new ContainedEntity();
+		containedEntity.setId( 3 );
+
+		entityManager.persist( containingEntity1 );
+		entityManager.persist( entity1 );
+		entityManager.persist( containedEntity );
+
+		containingEntity1.setContained( containedEntity );
+		containedEntity.setContaining( containingEntity1 );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -36,8 +57,95 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final ContainingEntity containing = entityManager.find( ContainingEntity.class, 2 );
+		final ContainedEntity containedEntity = containing.getContained();
+
+		entityManager.remove( containedEntity );
+		entityManager.flush();
+
+		final ContainingEntity parent = containing.getParent();
+		final ContainingEntity child = parent.getChild();
+		assertThat( child.getId() ).isEqualTo( 2 );
+		assertThat( child ).isSameAs( containing );
+		assertThat( child.getContained() ).isNull();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+
+	@Entity( name = "ContainingEntity" )
+	static class ContainingEntity {
+		@Id
+		private Integer id;
+
+		@OneToOne( fetch = FetchType.LAZY )
+		private ContainingEntity parent;
+
+		@OneToOne( mappedBy = "parent", fetch = FetchType.LAZY )
+		private ContainingEntity child;
+
+		@OneToOne( mappedBy = "containing" )
+		private ContainedEntity contained;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public ContainingEntity getParent() {
+			return parent;
+		}
+
+		public void setParent(ContainingEntity parent) {
+			this.parent = parent;
+		}
+
+		public ContainingEntity getChild() {
+			return child;
+		}
+
+		public void setChild(ContainingEntity child) {
+			this.child = child;
+		}
+
+		public ContainedEntity getContained() {
+			return contained;
+		}
+
+		public void setContained(ContainedEntity contained) {
+			this.contained = contained;
+		}
+
+	}
+
+	@Entity( name = "ContainedEntity" )
+	static class ContainedEntity {
+		@Id
+		private Integer id;
+
+		@OneToOne( fetch = FetchType.LAZY )
+		@JoinColumn( name = "containing" )
+		private ContainingEntity containing;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public ContainingEntity getContaining() {
+			return containing;
+		}
+
+		public void setContaining(ContainingEntity containing) {
+			this.containing = containing;
+		}
 	}
 }
