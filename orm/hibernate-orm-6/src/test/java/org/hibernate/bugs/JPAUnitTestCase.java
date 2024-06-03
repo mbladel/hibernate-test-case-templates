3c85649e5d8c0ce95a18d6c272762567cc8c200f
Marco Belladelli <marcobladel@gmail.com>
2024-06-03 09:46:37 +0200

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,19 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
+import java.io.Serializable;
+
+import org.hibernate.annotations.Imported;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -36,8 +43,52 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery(
+				"select new MyConcreteDto(e.id, e.someOtherField) from MyConcreteEntity e",
+				MyConcreteDto.class
+		).getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@MappedSuperclass
+	static abstract class AbstractEntity<K extends Serializable> {
+		@Id
+		protected K id;
+
+		protected K getId() {
+			return id;
+		}
+
+		protected void setId(K id) {
+			this.id = id;
+		}
+	}
+
+	@Entity( name = "MyConcreteEntity" )
+	static class MyConcreteEntity extends AbstractEntity<Long> {
+		protected String someOtherField;
+
+		public String getSomeOtherField() {
+			return someOtherField;
+		}
+
+		public void setSomeOtherField(String someOtherField) {
+			this.someOtherField = someOtherField;
+		}
+	}
+
+	@Imported
+	public static class MyConcreteDto {
+		protected Long id;
+
+		protected String someOtherField;
+
+		public MyConcreteDto(Long id, String someOtherField) {
+			this.id = id;
+			this.someOtherField = someOtherField;
+		}
 	}
 }
