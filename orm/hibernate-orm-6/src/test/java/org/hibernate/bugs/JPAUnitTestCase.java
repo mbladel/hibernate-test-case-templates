615a0ce7ff6c3951c1ce8457d7d8478f126e6831
Marco Belladelli <marcobladel@gmail.com>
2024-06-12 16:23:00 +0200

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,20 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -36,8 +44,26 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		final Child child = new Child();
+		child.id = 1L;
+		entityManager.persist( child );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Parent" )
+	@Inheritance( strategy = InheritanceType.SINGLE_TABLE )
+	static class Parent {
+		@Id
+		protected Long id;
+	}
+
+	@Entity( name = "Child" )
+	static class Child extends Parent {
+		@Generated( event = EventType.INSERT )
+		@Column( unique = true, insertable = false, updatable = false )
+		protected Integer someGenerated;
 	}
 }
