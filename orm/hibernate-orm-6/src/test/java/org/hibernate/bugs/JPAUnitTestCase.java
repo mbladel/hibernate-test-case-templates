6222be0e3bb665f2d5dc856359b53ffe6ee3a6ce
Marco Belladelli <marcobladel@gmail.com>
2024-07-09 08:44:06 +0200

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,18 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -36,8 +42,60 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery(
+				"select ef from BugEquipmentFeature ef" +
+						"left outer join fetch ef.id.feature f" +
+						"left outer join fetch f.id.equipmentClass", BugEquipmentFeature.class ).getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "BugEquipment" )
+	static class BugEquipment {
+		@Id
+		private Long id;
+	}
+
+	@Entity( name = "BugEquipmentClass" )
+	static class BugEquipmentClass {
+		@Id
+		private String id;
+	}
+
+	@Entity( name = "BugEquipmentFeature" )
+	static class BugEquipmentFeature {
+		@EmbeddedId
+		private BugEquipmentFeatureId id;
+	}
+
+	@Embeddable
+	static class BugEquipmentFeatureId {
+		@ManyToOne( cascade = {},        // cascade nothing
+				fetch = FetchType.LAZY,
+				optional = false )
+		private BugEquipment equipment;
+
+		@ManyToOne( cascade = {},        // cascade nothing
+				fetch = FetchType.LAZY,
+				optional = false )
+		private BugFeature feature;
+	}
+
+	@Entity( name = "BugFeature" )
+	static class BugFeature {
+		@EmbeddedId
+		private BugFeatureId id;
+	}
+
+	@Embeddable
+	static class BugFeatureId {
+		@ManyToOne( cascade = {},        // cascade nothing
+				fetch = FetchType.LAZY,
+				optional = false )
+		private BugEquipmentClass equipmentClass;
+
+		private String featureName;
 	}
 }
