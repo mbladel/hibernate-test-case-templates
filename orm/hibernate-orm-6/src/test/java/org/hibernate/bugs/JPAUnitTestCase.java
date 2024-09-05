263716ca91fdb600fa3791c5b7032ff81f20d27d
Marco Belladelli <marcobladel@gmail.com>
2024-09-05 12:11:14 +0200

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,17 +1,23 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Table;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -41,8 +47,40 @@ class JPAUnitTestCase {
 	void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Parent> cr = cb.createQuery( Parent.class );
+		Root<Parent> parent = cr.from( Parent.class );
+		parent.fetch( "association" );
+//		List<Parent> resultList = entityManager.createQuery( cr ).getResultList();
+
+		entityManager.getMetamodel()
+				.managedType( Parent.class )
+				.getAttribute( "association" );
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity( name = "Parent" )
+	@Table( name = "parent_test" )
+	@Inheritance
+	static class Parent {
+		@Id
+		Long id;
+	}
+
+	@Entity( name = "Child" )
+	@DiscriminatorValue( "Child" )
+	static class Child extends Parent {
+		@OneToOne
+		Association association;
+	}
+
+	@Entity( name = "Association" )
+	@Table( name = "association_test" )
+	static class Association {
+		@Id
+		Long id;
 	}
 }
