b282b63dfa677094c3526c7f9cbec604f14bfa1e
Marco Belladelli <marcobladel@gmail.com>
2024-10-29 13:18:31 +0100

    tests

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,17 +1,26 @@
 package org.hibernate.bugs;
 
-import java.util.*;
+import java.util.Map;
 
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
+import org.hibernate.Session;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.query.MutationQuery;
+import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.type.SqlTypes;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.CriteriaUpdate;
+import jakarta.persistence.criteria.Root;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -23,8 +32,10 @@ class JPAUnitTestCase {
 	@BeforeEach
 	void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+
+		entityManager.persist( new Foo( "id-1", Map.of( "key-1", 1234 ) ) );
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -39,10 +50,69 @@ class JPAUnitTestCase {
 	// Add your tests, using standard JUnit.
 	@Test
 	void hhh123Test() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		final Session session = entityManager.unwrap( Session.class );
+
+		{
+			final HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+			final CriteriaUpdate<Foo> criteria = builder.createCriteriaUpdate( Foo.class );
+			final Root<Foo> root = criteria.from( Foo.class );
+			criteria.set( root.get( "bar" ), Map.of( "key-2", 88888888 ) );
+			criteria.where( builder.equal( root.get( "id" ), "id-1" ) );
+			final MutationQuery query = session.createMutationQuery( criteria );
+			final int count = query.executeUpdate();
+
+			System.out.println( count + " rows updated" );
+		}
+
+		{
+			HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<Foo> criteria = builder.createQuery( Foo.class );
+			Root<Foo> root = criteria.from( Foo.class );
+			criteria.select( root );
+			criteria.where( builder.equal( root.get( "id" ), "id-1" ) );
+			Query<Foo> query = session.createQuery( criteria );
+			Foo foo = query.getSingleResult();
+
+			System.out.println( foo.getBar() );
+		}
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
+
+	@Entity(name = "Foo")
+	static class Foo {
+		@Id
+		private String id;
+
+		@JdbcTypeCode(SqlTypes.JSON)
+		private Map<String, Object> bar;
+
+		public Foo(String id, Map<String, Object> bar) {
+			this.id = id;
+			this.bar = bar;
+		}
+
+		public Foo() {
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public Map<String, Object> getBar() {
+			return bar;
+		}
+
+		public void setBar(Map<String, Object> bar) {
+			this.bar = bar;
+		}
+	}
+
 }
