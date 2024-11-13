6d90d6fa8cc8b383c96421c1b94044c9646a547f
Marco Belladelli <marcobladel@gmail.com>
2024-11-13 10:02:55 +0100

    tests

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,15 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-import javax.persistence.*;
-import javax.persistence.criteria.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Persistence;
+import javax.persistence.SecondaryTable;
 
 import org.junit.After;
 import org.junit.Before;
@@ -36,8 +43,57 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery( "update SecondaryTableEntitySub e set e.b=:b, e.c=:c" )
+				.setParameter( "b", 1 )
+				.setParameter( "c", 2 )
+				.executeUpdate();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "SecondaryTableEntityBase")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	public class SecondaryTableEntityBase {
+
+		private Long id;
+
+		@Id
+		@GeneratedValue
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity(name = "SecondaryTableEntitySub")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	@SecondaryTable(name = "test")
+	public class SecondaryTableEntitySub extends SecondaryTableEntityBase {
+		private Long b;
+
+		private Long c;
+
+		@Column
+		public Long getB() {
+			return b;
+		}
+
+		public void setB(Long b) {
+			this.b = b;
+		}
+
+		@Column(table = "test")
+		public Long getC() {
+			return c;
+		}
+
+		public void setC(Long c) {
+			this.c = c;
+		}
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,17 +1,19 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.SecondaryTable;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -41,8 +43,57 @@ class JPAUnitTestCase {
 	void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery( "update SecondaryTableEntitySub e set e.b=:b, e.c=:c" )
+				.setParameter( "b", 1 )
+				.setParameter( "c", 2 )
+				.executeUpdate();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "SecondaryTableEntityBase")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	public class SecondaryTableEntityBase {
+
+		private Long id;
+
+		@Id
+		@GeneratedValue
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity(name = "SecondaryTableEntitySub")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	@SecondaryTable(name = "test")
+	public class SecondaryTableEntitySub extends SecondaryTableEntityBase {
+		private Long b;
+
+		private Long c;
+
+		@Column
+		public Long getB() {
+			return b;
+		}
+
+		public void setB(Long b) {
+			this.b = b;
+		}
+
+		@Column(table = "test")
+		public Long getC() {
+			return c;
+		}
+
+		public void setC(Long c) {
+			this.c = c;
+		}
 	}
 }
