a869098bf432ee81aaf4acabce9473f2f66c40bc
Marco Belladelli <marcobladel@gmail.com>
2024-12-09 15:25:17 +0100

    tests

---
+++ orm/hibernate-orm-6/src/test/java/example/DerDA.java
@@ -0,0 +1,20 @@
+package example;
+
+
+import jakarta.persistence.Embeddable;
+
+/*
+ * Created on 03/12/2024 by Paul Harrison (paul.harrison@manchester.ac.uk).
+ */
+@Embeddable
+public class DerDA extends BaseD {
+  public DerDA(String a, String bprop) {
+    super(bprop);
+    this.a = a;
+  }
+
+  public String a;
+
+  public DerDA() {
+  }
+}
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,17 +1,20 @@
 package org.hibernate.bugs;
 
-import java.util.*;
-
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import example.DerDA;
+import example.DerDB;
+import example.DerOA;
+import example.DerOB;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -23,11 +26,16 @@ class JPAUnitTestCase {
 	@BeforeEach
 	void init() {
 		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
+		final EntityManager em = entityManagerFactory.createEntityManager();
+		em.getTransaction().begin();
 
-		entityManager.getTransaction().commit();
-		entityManager.close();
+		final DerDB derba1 = new DerDB( 5);
+		final DerDA derda1 = new DerDA( "1", "abase");
+		deroa = new DerOA(derda1);
+		derob  = new DerOB(derba1);
+
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@AfterEach
@@ -35,14 +43,40 @@ class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
+	DerOA deroa;
+	DerOB derob;
+
 	@Test
-	void hhh123Test() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
-		entityManager.close();
+	void testBaseProperty() {
+		final EntityManager em = entityManagerFactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist( deroa );
+		em.persist( derob );
+		em.getTransaction().commit();
+		final Integer ida = deroa.getId();
+		final Integer idb = derob.getId();
+
+		em.clear();
+		final TypedQuery<DerOA> qa = em.createQuery( "select o from DerOA o where o.id =:id", DerOA.class );
+		qa.setParameter( "id", ida );
+		final DerOA deroain = qa.getSingleResult();
+		assertEquals( "abase", deroain.derda.baseprop );
+	}
+
+	@Test
+	void testDerivedProperty() {
+		final EntityManager em = entityManagerFactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist( deroa );
+		em.persist( derob );
+		em.getTransaction().commit();
+		final Integer idb = derob.getId();
+		em.clear();
+
+		final TypedQuery<DerOB> qb = em.createQuery( "select o from DerOB o where o.id =:id", DerOB.class );
+		qb.setParameter( "id", idb );
+		final DerOB derobin = qb.getSingleResult();
+		assertNotNull( derobin );
+		assertEquals( 5, derobin.derdb().b );
 	}
 }
